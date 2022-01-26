fe457afc89c2b13d29fa66adf151147fe4f7889a
Sebastian Drejkarz <sdrejkarz90@gmail.com>
2022-01-26 19:39:54 +0100

    push project schema

---
+++ components/menu/index.tsx
@@ -0,0 +1,14 @@
+const MenuIndex = () => {
+  return (
+    <div style={{ display: "flex", backgroundColor:'red' }}>
+      <p>logo</p>
+      <p>Products</p>
+      <p>Solutions</p>
+      <p>Resources</p>
+      <p>About</p>
+      <button>Contact us</button>
+    </div>
+  )
+}
+
+export default MenuIndex
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['pl.wikipedia.org','upload.wikimedia.org'],
+  },
 }
 
 module.exports = nextConfig
